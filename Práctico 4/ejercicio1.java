b7f081e01d2bfd06dd5bae8b8dc4cf5fcd8f03d9
rm <mesiano2@hotmail.com>
2020-09-04 21:29:29 -0300

    Practico4/ej 3 punto 17

---
+++ Práctico 4/ejercicio1.java
@@ -0,0 +1,26 @@
+package clase3;
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+    //Método que reciba dos números y retorne verdadero
+    // si el primer numero es mayor que el segundo.
+    // De lo contrario, retornar debe retornar falso.
+
+    public static void main(String args []) {
+       
+    }
+    public static boolean elMayorPrimeroQueSegundo (int num1, int num2){
+            if (num1 > num2) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+
+
+
